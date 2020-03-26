71b843cd4cfe70e333390120c5e5b1267f782466
Dmitrii <dimon.shkarupa@gmail.com>
2020-03-26 15:03:47 +0200

    Creating hash function and methods size() empty() clear() normalize()

--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -30,7 +30,17 @@ private:
 	node* next_;
 };
 
-
+template<typename T>
+long long get_hash(T value) {
+	hash<T> hash_fn;
+	uint32_t h = hash_fn(value);
+	h = (h ^ 61) ^ (h >> 16);
+	h += (h << 3);
+	h ^= (h >> 4);
+	h *= 0x27d4eb2d;
+	h ^= (h >> 15);
+	return h;
+}
 
 template<typename K, typename V>
 class hash_table {
@@ -55,6 +65,45 @@ public:
 			table_[i] = nullptr;
 		}
 	}
+
+	explicit hash_table(const double load_factor, const int capacity) :
+		load_factor_(load_factor), capacity_(capacity) {
+
+		if (capacity < d_capacity_) capacity_ = d_capacity_;
+		if (load_factor < 0 || load_factor > 1)
+			load_factor_ = d_load_factor_;
+		table_ = new node<K, V> * [capacity_];
+		m_amount_ = capacity_ * load_factor_;
+
+		for (auto i = 0; i < capacity_; ++i) {
+			table_[i] = nullptr;
+		}
+	}
+
+	int size() const { return size_; }
+
+	int empty() const { return !size(); }
+
+	int normalize(const uint32_t key_hash) const {
+		return key_hash % capacity_;
+	}
+
+	void clear() {
+		for (auto i = 0; i < capacity_; ++i) {
+			if (table_[i] == nullptr) continue;
+			auto* traverse = table_[i];
+
+			while (traverse != nullptr) {
+				auto* item = traverse;
+				traverse = traverse->get_next();
+				free(item);
+			}
+
+			table_[i] = nullptr;
+		}
+		size_ = 0;
+	}
+	
 private:
 	const int d_capacity_ = 6;
 	const double d_load_factor_ = 0.8;
@@ -65,6 +114,7 @@ private:
 
 	node<K, V>** table_;
 };
+
 int main()
 {
 	return 0;
